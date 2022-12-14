fcbaef9ba4eee1c3a720662838d0871ac7a37f81
Programapython <106550324+Programapython@users.noreply.github.com>
2022-12-14 12:15:47 -0500

    .

--- src/app/productservice.ts
+++ src/app/productservice.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { Product } from './product';
+import { User } from './User';
 
 @Injectable()
 export class ProductService {
@@ -10,36 +10,8 @@ export class ProductService {
   productNames: string[] = [
     'Jesús Javier',
     'Luis Gustavo',
-    'Blue Band',
-    'Blue T-Shirt',
-    'Bracelet',
-    'Brown Purse',
-    'Chakra Bracelet',
-    'Galaxy Earrings',
-    'Game Controller',
-    'Gaming Set',
-    'Gold Phone Case',
-    'Green Earbuds',
-    'Green T-Shirt',
-    'Grey T-Shirt',
-    'Headphones',
-    'Light Green T-Shirt',
-    'Lime Band',
-    'Mini Speakers',
-    'Painted Phone Case',
-    'Pink Band',
-    'Pink Purse',
-    'Purple Band',
-    'Purple Gemstone Necklace',
-    'Purple T-Shirt',
-    'Shoes',
-    'Sneakers',
-    'Teal T-Shirt',
-    'Yellow Earbuds',
-    'Yoga Mat',
-    'Yoga Set',
   ];
-
+  
   constructor(private http: HttpClient) {}
 
   getProductsSmall() {
