f64e2ad4b7f271a46d0af3ddf53af3e24b0b2ec6
GJavierS <106267337+GJavierS@users.noreply.github.com>
2022-12-14 11:59:29 -0500

    Update product.ts

--- src/app/product.ts
+++ src/app/product.ts
@@ -1,12 +1,9 @@
 export interface Product {
     id?:string;
-    code?:string;
-    name?:string;
-    description?:string;
-    price?:number;
-    quantity?:number;
-    inventoryStatus?:string;
-    category?:string;
-    image?:string;
-    rating?:number;
+    dni?:number;
+    apellidos?:string;
+    nombres?:string;
+    edad?:number;
+    salario?:number;
+    departamento?:string;
 }
